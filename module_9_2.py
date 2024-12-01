86270fad8d792cfdbfc71a77600224961ae06b68
Yulya-Z <zukovayulya@gmail.com>
2024-12-01 16:23:00 +0300

    Add files via upload

---
+++ module_9_2.py
@@ -0,0 +1,10 @@
+first_strings = ['Elon', 'Musk', 'Programmer', 'Monitors', 'Variable']
+second_strings = ['Task', 'Git', 'Comprehension', 'Java', 'Computer', 'Assembler']
+
+first_result = [len(string) for string in first_strings if len(string) >= 5]
+second_result = [(s1, s2) for s1 in first_strings for s2 in second_strings if len(s1) == len(s2)]
+third_result = {string: len(string) for string in (first_strings + second_strings) if not len(string) % 2}
+
+print(first_result)
+print(second_result)
+print(third_result)
