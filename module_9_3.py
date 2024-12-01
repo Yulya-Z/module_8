0473e6149d2c903ee2cda030c4d797c6032b06db
Yulya-Z <zukovayulya@gmail.com>
2024-12-01 21:59:24 +0300

    Add files via upload

---
+++ module_9_3.py
@@ -0,0 +1,7 @@
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+first_result = (len(first) - len(second) for first, second in zip(first, second) if first != second)
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+print(list(first_result))
+print(list(second_result))
