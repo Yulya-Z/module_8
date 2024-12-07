732c57c4f5c4da91fec7e5525f304ebfca6719b4
Yulya-Z <zukovayulya@gmail.com>
2024-12-07 21:59:44 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,19 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if result > 1:
+            for i in range(2, result):
+                if (result % i) == 0:
+                    print(f"Составно {result}")
+            else:
+                print('Простое')
+                print(result)
+    return wrapper
+
+@is_prime
+def sum_three(a, b, c):
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
+
